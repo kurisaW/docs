d7eee0886d06c46dcb09da5fbf91b73f159af708
kurisaw <2053731441@qq.com>
2025-07-19 13:16:39 +0800

    2

--- docs/version-switcher.js
+++ docs/version-switcher.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('/versions.json')
-    .then(res => res.json())
-    .then(data => {
-      const currentPath = window.location.pathname.split('/')[1] || 'latest';
-      const switcher = `
-        <div id="version-switcher">
-          <select onchange="location.href='/' + this.value">
-            ${data.versions.map(v => 
-              `<option value="${v}" ${v === currentPath ? 'selected' : ''}>${v}</option>`
-            ).join('')}
-          </select>
-        </div>
-      `;
-      document.body.insertAdjacentHTML('afterbegin', switcher);
-    });
-});
+fetch('/versions.json')
+  .then(res => res.json())
+  .then(data => {
+    const currentVersion = window.location.pathname.split('/')[1] || 'latest';
+    const switcher = `
+      <div id="version-switcher">
+        <select onchange="location.href='/' + this.value + '/'">
+          ${data.versions.map(v => 
+            `<option value="${v}" ${v === currentVersion ? 'selected' : ''}>
+              ${v === 'latest' ? 'main' : v}
+            </option>`
+          ).join('')}
+        </select>
+      </div>
+    `;
+    document.body.insertAdjacentHTML('afterbegin', switcher);
+  });
